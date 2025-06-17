fbfdb590218581d2cde7715080e9104b5359f4be
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-17 15:00:52 -0300

    refactor: alteracao no metodo addCorsMappings alterando de allowedOrigins para allowedOriginPatterns

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/core/web/WebConfig.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/core/web/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                .allowedOrigins("*") // You can specify allowed origins here
+                .allowedOriginPatterns("*") // You can specify allowed origins here
                 .allowCredentials(true);
     }
 

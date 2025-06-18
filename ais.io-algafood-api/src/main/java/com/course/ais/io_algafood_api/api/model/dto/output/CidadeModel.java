4984621dc62490541f66fcf255a1229f9438bf7c
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-18 11:28:59 -0300

    feat: adicionando o RepresentationModel do hateoas

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/model/dto/output/CidadeModel.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/model/dto/output/CidadeModel.java
@@ -2,10 +2,13 @@ package com.course.ais.io_algafood_api.api.model.dto.output;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
 @Setter
 @Getter
-public class CidadeModel {
+@Relation(collectionRelation = "cidades") // Define o nome da coleção e do item na representação HATEOAS
+public class CidadeModel extends RepresentationModel<CidadeModel> {
 
     private Long id;
     private String nome;

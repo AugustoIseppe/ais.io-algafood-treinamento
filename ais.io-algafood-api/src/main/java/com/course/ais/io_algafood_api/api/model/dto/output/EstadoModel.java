0941f744cebad359c4e89699d627ca739e3dcad1
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-18 11:29:34 -0300

    refactor: adicionando o RepresentationModel do hateoas

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/model/dto/output/EstadoModel.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/api/model/dto/output/EstadoModel.java
@@ -2,10 +2,11 @@ package com.course.ais.io_algafood_api.api.model.dto.output;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
 @Setter
 @Getter
-public class EstadoModel {
+public class EstadoModel extends RepresentationModel<EstadoModel> {
 
     private Long id;
     private String nome;

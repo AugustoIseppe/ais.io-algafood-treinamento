d0655080d6718fe82bc0a3f1848d26e16f99d88c
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-22 17:12:48 -0300

    feat: implementando o PasswordEncoder

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/core/security/WebSecurityConfiguration.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/core/security/WebSecurityConfiguration.java
@@ -2,18 +2,14 @@ package com.course.ais.io_algafood_api.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 public class WebSecurityConfiguration {
-
+// No codigo do Nelio Alves o nome esta como SecyrityConfig
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable());
@@ -22,7 +18,7 @@ public class WebSecurityConfiguration {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder(); // Usado para codificar senhas
     }
 }

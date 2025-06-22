a986c1f9758f293aabc1b4994f4e22b1477e9a26
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-22 13:58:00 -0300

    feat: add spring security config

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/core/security/WebSecurityConfiguration.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/core/security/WebSecurityConfiguration.java
@@ -11,18 +11,13 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@EnableWebSecurity
 @Configuration
 public class WebSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/cozinhas").permitAll() // Permite acesso apenas ao endpoint /cozinhas, o resto requer autenticação (Basic Auth)
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 

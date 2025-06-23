9037c680c4c2bb021c2bfc122b90a1aef6f569d2
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2025-06-23 20:35:25 -0300

    refactor: alteracao no usuario repository

--- ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/repository/UsuarioRepository.java
+++ ais.io-algafood-api/src/main/java/com/course/ais/io_algafood_api/domain/repository/UsuarioRepository.java
@@ -2,13 +2,16 @@ package com.course.ais.io_algafood_api.domain.repository;
 
 import com.course.ais.io_algafood_api.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    Optional<Usuario> findByEmail(String email);
+    Usuario findByEmail(String email);
+
 
 }
